3b0f871968c154fe6fbb1214017d20f30afec682
Lydia <lydia.will4116@gmail.com>
2026-04-02 15:16:09 -0400

    sectioned maps

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ jan_Medicaid_filtered = jan_Medicaid.filter(pl.col("Final Report") == "Y")
 #TODO: Change maps to map by year. (edit first 3 lines of map function, and map_df's merge.
 # Can change map function to loop through all possible years)
 
+# --- Load US states ---
+states = gpd.read_file("https://naturalearth.s3.amazonaws.com/50m_cultural/ne_50m_admin_1_states_provinces.zip")
+states = states[states["admin"] == "United States of America"]
+
 def make_map(new_map, column, name, color):
     jan_total = (jan_Medicaid_filtered.group_by("State Name").agg(
         pl.col(column).sum()))
     jan_total_pd = jan_total.to_pandas()
-
-    # --- Load US states ---
-    states = gpd.read_file("https://naturalearth.s3.amazonaws.com/50m_cultural/ne_50m_admin_1_states_provinces.zip")
-    states = states[states["admin"] == "United States of America"]
 
     # Merge the formatted column too
     map_df = states.merge(
@@ -53,25 +53,28 @@ def make_map(new_map, column, name, color):
 
 
 # Create base map
-m = folium.Map(location=[37, -96], zoom_start=4, tiles="CartoDB positron")
+applications_map = folium.Map(location=[37, -96], zoom_start=4, tiles="CartoDB positron")
 
 #Add Map Layers here
-make_map(m, "Total Applications for Financial Assistance Submitted at State Level", "TOTAL Medicaid + Chip Applications", "Reds")
-make_map(m, "New Applications Submitted to Medicaid and CHIP Agencies", "NEW Medicaid + Chip Applications", "Reds")
-make_map(m, "Applications for Financial Assistance Submitted to the State Based Marketplace", "Applications to State Marketplace", "Reds")
-make_map(m, "Individuals Determined Eligible for Medicaid at Application", "Eligible Individuals", "Blues")
+make_map(applications_map, "Total Applications for Financial Assistance Submitted at State Level", "TOTAL Medicaid + Chip Applications", "Reds")
+make_map(applications_map, "New Applications Submitted to Medicaid and CHIP Agencies", "NEW Medicaid + Chip Applications", "Reds")
+make_map(applications_map, "Applications for Financial Assistance Submitted to the State Based Marketplace", "Applications to State Marketplace", "Reds")
+folium.LayerControl(collapsed=False).add_to(applications_map)
+applications_map.save("medicaid_applications_map.html")  # save to HTML
 
-make_map(m, "Total Medicaid and CHIP Determinations", "Total Medicaid and CHIP Determinations", "Blues")
-make_map(m, "Medicaid and CHIP Child Enrollment", "Medicaid and CHIP Child Enrollment", "Blues")
-make_map(m, "Total Medicaid and CHIP Enrollment", "Total Medicaid and CHIP Enrollment", "Blues")
-make_map(m, "Total Medicaid Enrollment", "Total Medicaid Enrollment", "Blues")
-make_map(m, "Total CHIP Enrollment", "Total CHIP Enrollment", "Blues")
 
-# Add layer control ONCE
-folium.LayerControl(collapsed=False).add_to(m)
+enrollment_map = folium.Map(location=[37, -96], zoom_start=4, tiles="CartoDB positron")
 
-#save and open map in webbrowser
-m.save("medicaid_january_map.html")  # save to HTML
-webbrowser.open("medicaid_january_map.html", new=2, autoraise=True)  # open in default browser
+make_map(enrollment_map, "Individuals Determined Eligible for Medicaid at Application", "Eligible Individuals", "Blues")
+make_map(enrollment_map, "Total Medicaid and CHIP Determinations", "Total Medicaid and CHIP Determinations", "Blues")
+make_map(enrollment_map, "Medicaid and CHIP Child Enrollment", "Medicaid and CHIP Child Enrollment", "Blues")
+make_map(enrollment_map, "Total Medicaid and CHIP Enrollment", "Total Medicaid and CHIP Enrollment", "Blues")
+make_map(enrollment_map, "Total Medicaid Enrollment", "Total Medicaid Enrollment", "Blues")
+make_map(enrollment_map, "Total CHIP Enrollment", "Total CHIP Enrollment", "Blues")
+folium.LayerControl(collapsed=False).add_to(enrollment_map)
+enrollment_map.save("medicaid_enrollment_map.html")  # save to HTML
+
+webbrowser.open("medicaid_applications_map.html", new=2, autoraise=True)  # open in default browser
+webbrowser.open("medicaid_enrollment_map.html", new=2, autoraise=True)  # open in default browser
 
 
